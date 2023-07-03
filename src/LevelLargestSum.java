cdced5fcc4f4f2bff4322df6a57bbcd09d493062
Yana <yanav@campus.technion.acil>
2023-07-03 13:36:05 +0300

    Binary tree - finished

--- src/LevelLargestSum.java
+++ src/LevelLargestSum.java
@@ -1,6 +1,40 @@
-public class LevelLargestSum {
-    /*public static int getLevelWithLargestSum(BinNode<Integer> root) {
-        // TODO: Add your code for part A2 here...
-    }*/
-}
+import java.util.ArrayDeque;
 
+public class LevelLargestSum {
+    /***
+     * the class checks for the level with the highest sum of root values
+     * @param root a current root that is being checked for value
+     * @return the level number of which the root values sum is the highest, and -1 if the tree is empty
+     */
+    public static int getLevelWithLargestSum(BinNode<Integer> root){
+        if (root == null)
+            return -1;
+
+        int level=0, maxLevel=0, maxSum=0,sum,i,levelSize;
+        ArrayDeque<BinNode> levelData = new ArrayDeque<>();
+        levelData.add(root);
+
+        while (!levelData.isEmpty()){
+            levelSize = levelData.size();
+            sum = 0;
+
+            for (i=0; i<levelSize; i++){
+                BinNode<Integer> currRoot = levelData.poll();
+                sum += currRoot.getData();
+
+                if (currRoot.getLeft() != null)
+                    levelData.add(currRoot.getLeft());
+                if (currRoot.getRight() != null)
+                    levelData.add(currRoot.getRight());
+            }
+
+            if (sum > maxSum){
+                maxSum = sum;
+                maxLevel = level;
+            }
+
+            level++;
+        }
+        return maxLevel;
+    }
+}
--- src/PathFromRoot.java
+++ src/PathFromRoot.java
@@ -1,6 +1,19 @@
 public class PathFromRoot {
-    /*public static boolean doesPathExist(BinNode<Character> root, String str) {
-        // TODO: Add your code for part A1 here...
-    }*/
+    /***
+     * the method checks for a possible root path so that the root values form a String identical to the given String
+     * @param root a current root that is being checked
+     * @param str the given String
+     * @return True if such path exists , False otherwise
+     */
+    public static boolean doesPathExist(BinNode<Character> root, String str) {
 
+        if(root==null || str.length()==0)
+            return true;
+
+        if(root.getData() != str.charAt(0))
+            return false;
+
+        return doesPathExist(root.getLeft(),str.substring(1))
+                || doesPathExist(root.getRight(),str.substring(1));
+    }
 }
